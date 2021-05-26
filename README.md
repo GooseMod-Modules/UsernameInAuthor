7cc74444a272f11886e56c4ce66f3a74efdc3689
Oj <oj@oojmed.com>
2021-05-26 19:11:52 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# UsernameInAuthor
+Adds username to message's author if they have a nickname.
---
+++ index.js
@@ -0,0 +1,28 @@
+import { username } from '@goosemod/patcher';
+import { findByProps } from '@goosemod/webpack';
+import { React } from '@goosemod/webpack/common';
+
+const MessageClasses = findByProps('compact', 'repliedMessage', 'username');
+
+let unpatch;
+
+export default {
+  goosemodHandlers: {
+    onImport: () => {
+      unpatch = username.patch(({ message, author }) =>
+        React.createElement('span', {
+          className: MessageClasses.username,
+          style: {
+            color: author.colorString,
+            filter: 'brightness(0.5)'
+          }
+        }, author.nick === message.author.username ? '' : `(${message.author.username})`
+        )
+      );
+    },
+
+    onRemove: () => {
+      unpatch();
+    }
+  }
+};
